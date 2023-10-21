505b81461d92ed8940f405b75ab4a439b93c71d0
izzulaverroes <izzulaverroes@gmail.com>
2023-10-21 09:21:51 +0700

    V4

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,18 @@ document.addEventListener('click',function(e){
         document.querySelector('.burger span:nth-child(4)').classList.remove('x3')
     }
 })
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
